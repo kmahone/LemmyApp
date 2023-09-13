52035b2f45fe7e2660e2d0ae1d63c5ad0985e2f8
Keith Mahoney <kmahone@microsoft.com>
2023-09-13 11:13:05 -0700

    basic image scroller

--- LemmyApp1/LemmyApp1/MainWindow.xaml.cs
+++ LemmyApp1/LemmyApp1/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Lemmy.Net;
+using Lemmy.Net.Types;
+using Microsoft.UI.Xaml.Media.Imaging;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -28,9 +31,63 @@ namespace LemmyApp1
             this.InitializeComponent();
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+        private async void myButton_Click(object sender, RoutedEventArgs e)
         {
-            myButton.Content = "Clicked";
+            var client = new LemmyHttp("https://lemmy.world");
+
+            GetCommunity getCommunity = new GetCommunity
+            {
+                Name = "pics"
+            };
+            var comRes = await client.GetCommunity(getCommunity);
+            var comView = comRes.CommunityView;
+            var com = comView.Community;
+            Log(com.Description);
+
+            GetPosts getPosts = new GetPosts
+            {
+                CommunityId = com.Id,
+                Limit = 40,
+            };
+            var getPostsRes = await client.GetPosts(getPosts);
+            foreach(var postView in getPostsRes.Posts)
+            {
+                var post = postView.Post;
+                Log(post.Name);
+                if(IsImageUrl(post.Url))
+                {
+                    //AddImage(post.Url);
+                }
+                else
+                {
+                    Log("Not an image: " + post.Url);
+                }
+            }
+
+            var imagePosts = getPostsRes.Posts.Select(postView => postView.Post).Where(post => IsImageUrl(post.Url)).ToList();
+            repeater.ItemsSource = imagePosts;
+        }
+
+        private bool IsImageUrl(string url)
+        {
+            if(string.IsNullOrEmpty(url)) return false; 
+
+            return url.EndsWith(".png") || url.EndsWith(".jpg") || url.EndsWith(".gif") || url.EndsWith(".jpeg");
+        }
+
+        private void Log(string message) 
+        {
+            tb1.Text += message + Environment.NewLine;
+        }
+
+        private void AddImage(string url)
+        {
+            Image image = new Image();
+            BitmapImage bitmap = new BitmapImage();
+            bitmap.UriSource = new Uri(url);
+            image.Source = bitmap;
+
+            sp1.Children.Add(image);
         }
     }
 }
