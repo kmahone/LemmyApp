43547a3e0ecbb22bec29d216000a4c5901fcd261
Keith Mahoney <kmahone@microsoft.com>
2023-09-13 11:44:32 -0700

    infinite image scrolling

--- LemmyApp1/LemmyApp1/MainWindow.xaml.cs
+++ LemmyApp1/LemmyApp1/MainWindow.xaml.cs
@@ -15,6 +15,8 @@ using Windows.Foundation.Collections;
 using Lemmy.Net;
 using Lemmy.Net.Types;
 using Microsoft.UI.Xaml.Media.Imaging;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -29,11 +31,46 @@ namespace LemmyApp1
         public MainWindow()
         {
             this.InitializeComponent();
+            scrollViewer1.ViewChanged += ScrollViewer1_ViewChanged;
         }
 
-        private async void myButton_Click(object sender, RoutedEventArgs e)
+        bool setupRan = false;
+        bool isLoadingMoreItems = false;
+
+        ObservableCollection<Post> imagePosts;
+        LemmyHttp client;
+        long communityId;
+        int page = 1;
+
+        async void LoadMoreItems()
         {
-            var client = new LemmyHttp("https://lemmy.world");
+            if(isLoadingMoreItems)
+            {
+                return;
+            }
+            isLoadingMoreItems = true;
+            Log("LoadMoreItems!");
+            page++;
+            GetPosts getPosts = new GetPosts
+            {
+                CommunityId = communityId,
+                Limit = 10,
+                Page = page,
+            };
+            var getPostsRes = await client.GetPosts(getPosts);
+
+            var imagePosts1 = getPostsRes.Posts.Select(postView => postView.Post).Where(post => IsImageUrl(post.Url));
+            foreach (var post in imagePosts1) 
+            {
+                imagePosts.Add(post);
+            }
+            isLoadingMoreItems = false;
+        }
+
+
+        private async Task Setup()
+        {
+            client = new LemmyHttp("https://lemmy.world");
 
             GetCommunity getCommunity = new GetCommunity
             {
@@ -42,19 +79,22 @@ namespace LemmyApp1
             var comRes = await client.GetCommunity(getCommunity);
             var comView = comRes.CommunityView;
             var com = comView.Community;
-            Log(com.Description);
+            //Log(com.Description);
+
+            communityId = com.Id;
 
             GetPosts getPosts = new GetPosts
             {
-                CommunityId = com.Id,
-                Limit = 40,
+                CommunityId = communityId,
+                Limit = 10,
+                Page = 1,
             };
             var getPostsRes = await client.GetPosts(getPosts);
-            foreach(var postView in getPostsRes.Posts)
+            foreach (var postView in getPostsRes.Posts)
             {
                 var post = postView.Post;
-                Log(post.Name);
-                if(IsImageUrl(post.Url))
+                //Log(post.Name);
+                if (IsImageUrl(post.Url))
                 {
                     //AddImage(post.Url);
                 }
@@ -64,8 +104,34 @@ namespace LemmyApp1
                 }
             }
 
-            var imagePosts = getPostsRes.Posts.Select(postView => postView.Post).Where(post => IsImageUrl(post.Url)).ToList();
+            var imagePosts1 = getPostsRes.Posts.Select(postView => postView.Post).Where(post => IsImageUrl(post.Url));
+            imagePosts = new ObservableCollection<Post>(imagePosts1);
             repeater.ItemsSource = imagePosts;
+
+            setupRan = true;
+        }
+
+
+        private void ScrollViewer1_ViewChanged(object sender, ScrollViewerViewChangedEventArgs e)
+        {
+            if(setupRan)
+            {
+                var offset = scrollViewer1.VerticalOffset;
+                var extent = scrollViewer1.ExtentHeight;
+                //Log($"ScrollViewer1_ViewChanged offset={offset}, extent={extent}, IsIntermediate={e.IsIntermediate} ");
+
+                var viewport = scrollViewer1.ViewportHeight;
+                var boundary = extent - (3 * viewport);
+                if (offset > boundary)
+                {
+                    LoadMoreItems();
+                }
+            }
+        }
+
+        private void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            Setup().GetAwaiter();
         }
 
         private bool IsImageUrl(string url)
@@ -82,12 +148,12 @@ namespace LemmyApp1
 
         private void AddImage(string url)
         {
-            Image image = new Image();
-            BitmapImage bitmap = new BitmapImage();
-            bitmap.UriSource = new Uri(url);
-            image.Source = bitmap;
+            //Image image = new Image();
+            //BitmapImage bitmap = new BitmapImage();
+            //bitmap.UriSource = new Uri(url);
+            //image.Source = bitmap;
 
-            sp1.Children.Add(image);
+            //sp1.Children.Add(image);
         }
     }
 }
